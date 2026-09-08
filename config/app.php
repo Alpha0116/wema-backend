ef9e008702ab4effd9f11ad6741d4b79db5c8317
KILOTROK <kilotrok@KILOTROKs-MacBook-Air.local>
2026-09-08 05:25:22 +0100

    fix: add built-in APP_KEY fallback in config/app.php

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:Ja6wWK6yoA4rIKYfSWBmNWan/LQdSmaxBAIgMEH91Eo='),
 
     'previous_keys' => [
         ...array_filter(
